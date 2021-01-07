435f6b42f750487a6b940606bc8be2b73a169df1
suzygiv <suzannegivnish@gmail.com>
2021-01-07 15:34:57 -0500

    placed aggregate within range

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 //use static files
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/immense-tundra-21564", 
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fittrack", 
 {
     useNewUrlParser: true, 
     useUnifiedTopology: true,
